800c2f6d9d7ec8c8a2950764c5424af7f1ae02b6
ManishPoduval <manishpoduval@hotmail.com>
2022-08-26 11:21:29 +0530

    setup

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function sum(a, b) {
+  return;
+}
+
+function subtract(a, b) {
+  return;
+}
+
+function divide(a, b) {
+  return;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// The following is required to make unit tests work. Please ignore it.
+module.exports = { sum, subtract, divide, multiply };
